5788cec1dc890d98d687a91a93de1c72bbf475c9
CoffeeCatRailway <dragonlee1002@gmail.com>
2021-05-03 21:33:09 +1000

    Set chopping board as villager chef poi

--- src/main/java/coffeecatrailway/hamncheese/registry/HNCProfessions.java
+++ src/main/java/coffeecatrailway/hamncheese/registry/HNCProfessions.java
@@ -2,6 +2,7 @@ package coffeecatrailway.hamncheese.registry;
 
 import coffeecatrailway.hamncheese.HNCMod;
 import coffeecatrailway.hamncheese.datagen.HNCLanguage;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -16,6 +17,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * @author CoffeeCatRailway
@@ -27,7 +30,10 @@ public class HNCProfessions
     protected static final DeferredRegister<PointOfInterestType> POI_TYPES = DeferredRegister.create(ForgeRegistries.POI_TYPES, HNCMod.MOD_ID);
     protected static final DeferredRegister<VillagerProfession> PROFESSIONS = DeferredRegister.create(ForgeRegistries.PROFESSIONS, HNCMod.MOD_ID);
 
-    public static final RegistryObject<PointOfInterestType> CHEF_POI = registerPoi("chef", PointOfInterestType.getBlockStates(Blocks.ANVIL), 1, 1); // Add kitchen block
+    private static final Supplier<Set<BlockState>> CHOPPING_BOARDS = () -> ImmutableList.of(HNCBlocks.OAK_CHOPPING_BOARD.get(), HNCBlocks.BIRCH_CHOPPING_BOARD.get(), HNCBlocks.SPRUCE_CHOPPING_BOARD.get(), HNCBlocks.JUNGLE_CHOPPING_BOARD.get(), HNCBlocks.ACACIA_CHOPPING_BOARD.get(), HNCBlocks.DARK_OAK_CHOPPING_BOARD.get(), HNCBlocks.WARPED_CHOPPING_BOARD.get(), HNCBlocks.CRIMSON_CHOPPING_BOARD.get(), HNCBlocks.STONE_CHOPPING_BOARD.get(), HNCBlocks.POLISHED_BLACKSTONE_CHOPPING_BOARD.get(), HNCBlocks.GOLD_CHOPPING_BOARD.get(), HNCBlocks.IRON_CHOPPING_BOARD.get()).stream()
+            .flatMap(board -> board.getStateDefinition().getPossibleStates().stream()).collect(Collectors.toSet());
+
+    public static final RegistryObject<PointOfInterestType> CHEF_POI = registerPoi("chef", CHOPPING_BOARDS, 1, 1);
     public static final RegistryObject<VillagerProfession> CHEF = registerProfession("chef", CHEF_POI, SoundEvents.VILLAGER_WORK_BUTCHER);
 
     private static RegistryObject<VillagerProfession> registerProfession(String id, RegistryObject<PointOfInterestType> poi, SoundEvent sound)
@@ -37,9 +43,9 @@ public class HNCProfessions
         return object;
     }
 
-    private static RegistryObject<PointOfInterestType> registerPoi(String id, Set<BlockState> matchingStates, int maxTickets, int validRange)
+    private static RegistryObject<PointOfInterestType> registerPoi(String id, Supplier<Set<BlockState>> matchingStates, int maxTickets, int validRange)
     {
-        return POI_TYPES.register(id, () -> new PointOfInterestType(id, matchingStates, maxTickets, validRange));
+        return POI_TYPES.register(id, () -> new PointOfInterestType(id, matchingStates.get(), maxTickets, validRange));
     }
 
     public static void load(IEventBus bus)
