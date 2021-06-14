17df0f4f5de4cf371d799a7666ba2b939b3c81a6
CoffeeCatRailway <dragonlee1002@gmail.com>
2021-06-14 19:04:15 +1000

    Removed un-needed render layers

--- src/main/java/coffeecatrailway/hamncheese/ClientEvents.java
+++ src/main/java/coffeecatrailway/hamncheese/ClientEvents.java
@@ -2,6 +2,7 @@ package coffeecatrailway.hamncheese;
 
 import coffeecatrailway.hamncheese.client.entity.MouseRenderer;
 import coffeecatrailway.hamncheese.client.gui.screen.PizzaOvenScreen;
+import coffeecatrailway.hamncheese.integration.registry.HNCBlocksBOP;
 import coffeecatrailway.hamncheese.registry.HNCBlocks;
 import coffeecatrailway.hamncheese.registry.HNCContainers;
 import coffeecatrailway.hamncheese.registry.HNCEntities;
@@ -10,6 +11,7 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -35,22 +37,6 @@ public class ClientEvents
 
         RenderTypeLookup.setRenderLayer(HNCBlocks.PINEAPPLE_PLANT.get(), cutoutMipped);
         RenderTypeLookup.setRenderLayer(HNCBlocks.TOMATO_PLANT.get(), cutoutMipped);
-
-        RenderTypeLookup.setRenderLayer(HNCBlocks.OAK_CHOPPING_BOARD.get(), cutoutMipped);
-        RenderTypeLookup.setRenderLayer(HNCBlocks.BIRCH_CHOPPING_BOARD.get(), cutoutMipped);
-        RenderTypeLookup.setRenderLayer(HNCBlocks.SPRUCE_CHOPPING_BOARD.get(), cutoutMipped);
-        RenderTypeLookup.setRenderLayer(HNCBlocks.JUNGLE_CHOPPING_BOARD.get(), cutoutMipped);
-        RenderTypeLookup.setRenderLayer(HNCBlocks.ACACIA_CHOPPING_BOARD.get(), cutoutMipped);
-        RenderTypeLookup.setRenderLayer(HNCBlocks.DARK_OAK_CHOPPING_BOARD.get(), cutoutMipped);
-        RenderTypeLookup.setRenderLayer(HNCBlocks.CRIMSON_CHOPPING_BOARD.get(), cutoutMipped);
-        RenderTypeLookup.setRenderLayer(HNCBlocks.WARPED_CHOPPING_BOARD.get(), cutoutMipped);
-
-        RenderTypeLookup.setRenderLayer(HNCBlocks.STONE_CHOPPING_BOARD.get(), cutoutMipped);
-        RenderTypeLookup.setRenderLayer(HNCBlocks.POLISHED_BLACKSTONE_CHOPPING_BOARD.get(), cutoutMipped);
-        RenderTypeLookup.setRenderLayer(HNCBlocks.GOLD_CHOPPING_BOARD.get(), cutoutMipped);
-        RenderTypeLookup.setRenderLayer(HNCBlocks.IRON_CHOPPING_BOARD.get(), cutoutMipped);
-
-        RenderTypeLookup.setRenderLayer(HNCBlocks.PIZZA_OVEN.get(), cutoutMipped);
     }
 
     private static void entityRenderers()
