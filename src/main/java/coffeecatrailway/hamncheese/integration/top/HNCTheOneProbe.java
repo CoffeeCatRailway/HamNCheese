66df599884ca34eba7c8591d8141909f16f9fc56
CoffeeCatRailway <dragonlee1002@gmail.com>
2021-08-30 13:01:06 +1000

    :sparkles: TOP now shows info for popcorn machines

--- src/main/java/coffeecatrailway/hamncheese/integration/top/HNCTheOneProbe.java
+++ src/main/java/coffeecatrailway/hamncheese/integration/top/HNCTheOneProbe.java
@@ -2,16 +2,21 @@ package coffeecatrailway.hamncheese.integration.top;
 
 import coffeecatrailway.hamncheese.HNCMod;
 import coffeecatrailway.hamncheese.common.block.ChoppingBoardBlock;
+import coffeecatrailway.hamncheese.common.item.crafting.PopcornRecipe;
 import coffeecatrailway.hamncheese.common.tileentity.GrillTileEntity;
 import coffeecatrailway.hamncheese.common.tileentity.PizzaOvenTileEntity;
+import coffeecatrailway.hamncheese.common.tileentity.PopcornMachineTileEntity;
+import coffeecatrailway.hamncheese.data.gen.HNCLanguage;
 import mcjty.theoneprobe.api.*;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
@@ -51,6 +56,17 @@ public class HNCTheOneProbe implements Function<ITheOneProbe, Void>, IProbeInfoP
             GrillTileEntity grill = (GrillTileEntity) tile;
             addProgress(info, 0xff301b00, 0xff916e2f, 0xffae8b4c, grill.data.get(2), grill.data.get(3));
         }
+        if (tile instanceof PopcornMachineTileEntity)
+        {
+            PopcornMachineTileEntity popcornMachine = (PopcornMachineTileEntity) tile;
+            ItemStack result = popcornMachine.getItem(PopcornMachineTileEntity.SLOT_DOWN);
+            if (!result.isEmpty())
+                info.item(result.copy());
+            if (popcornMachine.data.get(0) > 0)
+                addProgress(info, 0xffb26411, 0xfff5dd5d, 0xff752802, popcornMachine.data.get(0), PopcornMachineTileEntity.MAX_FLAVOUR_TIME);
+            if (popcornMachine.data.get(1) > 0)
+                addProgress(info, 0xffffffff, 0xfffae99f, 0xffce3723, popcornMachine.data.get(1), PopcornMachineTileEntity.MAX_POPCORN, style -> style.numberFormat(NumberFormat.NONE).prefix(HNCLanguage.getPopcorn(popcornMachine.data.get(1))));
+        }
 
         if (block instanceof ChoppingBoardBlock)
             info.text(new TranslationTextComponent("top.hamncheese.chop_chop"));
@@ -58,9 +74,14 @@ public class HNCTheOneProbe implements Function<ITheOneProbe, Void>, IProbeInfoP
 
     private static void addProgress(IProbeInfo info, int border, int filled, int background, float amount, float total)
     {
+        addProgress(info, border, filled, background, amount, total, style -> style.prefix(new TranslationTextComponent("top.hamncheese.progress")).suffix("%"));
+    }
+
+    private static void addProgress(IProbeInfo info, int border, int filled, int background, float amount, float total, Function<IProgressStyle, IProgressStyle> style)
+    {
         float progressTotal = 0f;
         if (amount > 0 && total > 0)
             progressTotal = amount / total * 100f;
-        info.progress((int) progressTotal, 100, info.defaultProgressStyle().copy().color(border, filled, background).alternateFilledColor(filled).prefix(new TranslationTextComponent("top.hamncheese.progress")).suffix("%"));
+        info.progress((int) progressTotal, 100, style.apply(info.defaultProgressStyle().copy().color(border, filled, background).alternateFilledColor(filled)));
     }
 }
