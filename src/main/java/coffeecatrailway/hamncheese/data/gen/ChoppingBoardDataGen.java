8057f35e2b29067dc452226236b2c9b729cc804c
CoffeeCatRailway <dragonlee1002@gmail.com>
2021-09-18 19:40:03 +1000

    :bento: Added missing chopping boards

--- src/main/java/coffeecatrailway/hamncheese/data/gen/ChoppingBoardDataGen.java
+++ src/main/java/coffeecatrailway/hamncheese/data/gen/ChoppingBoardDataGen.java
@@ -61,6 +61,44 @@ public class ChoppingBoardDataGen implements IDataProvider {
 
     private void start() {
         this.addChoppingBoard("oak_chopping_board", Blocks.OAK_PRESSURE_PLATE, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("birch_chopping_board", Blocks.BIRCH_PRESSURE_PLATE, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("spruce_chopping_board", Blocks.SPRUCE_PRESSURE_PLATE, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("jungle_chopping_board", Blocks.JUNGLE_PRESSURE_PLATE, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("acacia_chopping_board", Blocks.ACACIA_PRESSURE_PLATE, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("dark_oak_chopping_board", Blocks.DARK_OAK_PRESSURE_PLATE, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("crimson_chopping_board", Blocks.CRIMSON_PRESSURE_PLATE, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("warped_chopping_board", Blocks.WARPED_PRESSURE_PLATE, SoundEvents.WOOD_PLACE, "axe");
+
+        this.addChoppingBoard("stone_chopping_board", Blocks.STONE_PRESSURE_PLATE, SoundEvents.STONE_PLACE, "pickaxe");
+        this.addChoppingBoard("polished_blackstone_chopping_board", Blocks.POLISHED_BLACKSTONE_PRESSURE_PLATE, SoundEvents.STONE_PLACE, "pickaxe");
+        this.addChoppingBoard("light_weighted_chopping_board", Blocks.LIGHT_WEIGHTED_PRESSURE_PLATE, SoundEvents.METAL_PLACE, "pickaxe");
+        this.addChoppingBoard("heavy_weighted_chopping_board", Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE, SoundEvents.METAL_PLACE, "pickaxe");
+
+        this.addChoppingBoard("maple_chopping_board", HNCBlocks.MAPLE_PRESSURE_PLATE.get(), SoundEvents.WOOD_PLACE, "axe");
+
+        this.addChoppingBoard("fir_chopping_board", "biomesoplenty", BOPBlocks.fir_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("redwood_chopping_board", "biomesoplenty", BOPBlocks.redwood_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("cherry_chopping_board", "biomesoplenty", BOPBlocks.cherry_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("mahogany_chopping_board", "biomesoplenty", BOPBlocks.mahogany_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("jacaranda_chopping_board", "biomesoplenty", BOPBlocks.jacaranda_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("palm_chopping_board", "biomesoplenty", BOPBlocks.palm_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("willow_chopping_board", "biomesoplenty", BOPBlocks.willow_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("dead_chopping_board", "biomesoplenty", BOPBlocks.dead_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("magic_chopping_board", "biomesoplenty", BOPBlocks.magic_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("umbran_chopping_board", "biomesoplenty", BOPBlocks.umbran_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("hellbark_chopping_board", "biomesoplenty", BOPBlocks.hellbark_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+
+        this.addChoppingBoard("twilight_oak_plate_chopping_board", "twilightforest", TFBlocks.twilight_oak_plate.get(), SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("canopy_chopping_board", "twilightforest", TFBlocks.canopy_plate.get(), SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("mangrove_chopping_board", "twilightforest", TFBlocks.mangrove_plate.get(), SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("dark_chopping_board", "twilightforest", TFBlocks.dark_plate.get(), SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("time_chopping_board", "twilightforest", TFBlocks.time_plate.get(), SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("trans_chopping_board", "twilightforest", TFBlocks.trans_plate.get(), SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("mine_chopping_board", "twilightforest", TFBlocks.mine_plate.get(), SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("sort_chopping_board", "twilightforest", TFBlocks.sort_plate.get(), SoundEvents.WOOD_PLACE, "axe");
+
+        this.addChoppingBoard("darkwood_chopping_board", "druidcraft", BlockRegistry.darkwood_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
+        this.addChoppingBoard("elder_chopping_board", "druidcraft", BlockRegistry.elder_pressure_plate, SoundEvents.WOOD_PLACE, "axe");
     }
 
     private void addChoppingBoard(String name, Block pressurePlate, SoundEvent convertSound, String toolType) {
